46ff5c1c02deb3c0e140c41eb8207d871c8853ef
mihaSpb <mihaspb2007@gmail.com>
2019-04-05 00:57:42 +0300

    upload

---
+++ HomeWork_2/app/src/main/java/com/example/hp/homework_2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.hp.homework_2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView myText = findViewById(R.id.mainText);
+        myText.setMovementMethod(new ScrollingMovementMethod());
+
+        Button countButton = findViewById(R.id.buttonAll);
+        countButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), getString(R.string.text_toast), Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER, 0,0);
+                toast.show();
+            }
+        });
+    }
+}
